6590a89e7029a92c4a5b0ab05c2d0e531e97f9b4
keishn <vishinka121929@gmail.com>
2023-05-26 21:53:50 +0300

    first commit

---
+++ src/test/java/com/example/examDevelop/ExamDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examDevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
